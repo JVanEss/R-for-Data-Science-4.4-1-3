a246d1c3d63a063e5c62877965d88e11c241038c
Josie Van Ess <jvaness@redventures.com>
2018-10-28 20:50:51 -0400

    chapter 4 assignment

---
+++ 4 4.R
@@ -0,0 +1,23 @@
+1 / 200 * 30
+(59 + 73 + 2) / 3
+sin(pi / 2)
+#object_name <- value
+this_is_a_really_long_name <- 3.5
+this_is_a_really_long_name
+r_rocks <- 2 ^ 3
+#function_name(arg1 = val1, arg2 = val2, ...)
+seq(1, 10)
+x <- "hello world"
+(y <- seq(1, 10, length.out = 5))
+
+#4.4 Practice
+#Exercise 1
+#the "i" is not dotted so R does not recognize it as the same assignment
+#Exercise 2
+library(tidyverse)
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
+filter(mpg, cyl == 8)
+filter(diamonds, carat > 3)
+#Exercise 3
+#it opens a table of keyboard shortcuts
+#this can also be found through Tooks > Keyboard Shortcuts
